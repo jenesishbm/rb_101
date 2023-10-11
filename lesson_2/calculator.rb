0c78d70bbd915e1620ddae79307319e35332133a
Jenesis Merriman <jenesishbm@berkeley.edu>
2023-10-11 16:25:27 +0100

    refactored calculator.rb using walk through video

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -1,59 +1,102 @@
-=begin CALCULATOR PROGRAM
-Build a command line calculator program that does the following:
-
-asks for two numbers
-asks for the type of operation to perform: add, subtract, multiply or divide
-displays the result
-=end
+# CALCULATOR PROGRAM
 
 # define method to check valid number
 def valid_number?(number_string)
   number_string.to_i.to_s == number_string
 end
 
+# define prompt method to display prompts
+def prompt(message)
+  puts "=> #{message}"
+end
+
+# define method to convert operator to message
+def op_to_message(op)
+  case op
+  when '1' then "Adding"
+  when '2' then "Subtracting"
+  when '3' then "Multiplying"
+  when '4' then "Dividing"
+  end
+end
+
 # define variables outside of loops
-op = nil
+name = nil
 num1 = nil
 num2 = nil
+op = nil
+result = nil
 
-# collect number 1
-loop do
-  puts '>>Please enter your first number:'
-  num1 = gets.chomp
-  break if valid_number?(num1)
-  puts 'Invalid input. Only integers are allowed.'
+# get name
+loop do 
+  prompt('Welcome to Calculator! Enter your name:')
+  name = gets.chomp
+  break unless name.empty?
+  prompt('Make sure to enter your name.')
 end
 
-# collect number 2
-loop do
-  puts '>>Please enter your second number:'
-  num2 = gets.chomp
-  break if valid_number?(num2)
-  puts 'Invalid input. Only integers are allowed.'
-end
+prompt("Great! Hi #{name}")
 
-# collect operation
-loop do
-  puts '> What type of operation would you like to perform?'
-  puts "Type 'a' to add, 's' to subtract, 'm' to multiply or 'd' to divide."
-  op = gets.chomp.downcase
-    if op == 'd' && num2 == '0'
-      puts 'You cannot divide by 0. Please select a different operation.'
-    elsif %w(a s m d).include?(op)
-      break
-    else
-      puts 'Try again. Your answer was not a valid operation.'
+loop do # initialize main loop
+  loop do # collect number 1
+    prompt('Please enter your first number:')
+    num1 = gets.chomp
+    break if valid_number?(num1)
+    prompt('Invalid input. Only integers are allowed.')
+  end
+
+  # collect number 2
+  loop do
+    prompt('Please enter your second number:')
+    num2 = gets.chomp
+    break if valid_number?(num2)
+    prompt('Invalid input. Only integers are allowed.')
+  end
+
+  # collect operation
+  op_prompt = <<-MSG
+    What operation would you like to perform?
+    1) add
+    2) subtract
+    3) multiply
+    4) divide
+  MSG
+
+  prompt(op_prompt)
+    loop do
+      op = gets.chomp.downcase
+      if op == '4' && num2 == '0'
+        prompt('You cannot divide by 0. Please select a different operation.')
+      elsif %w(1 2 3 4).include?(op)
+        break
+      else
+        prompt('Try again. You must choose 1, 2, 3, or 4.')
+      end
     end
+
+    prompt("#{op_to_message(op)} #{num1} and #{num2}...")
+
+  # calculates corresponding output
+  result = 
+  case op
+  when '1'
+    num1.to_i + num2.to_i
+  when '2'
+    num1.to_i - num2.to_i
+  when '3'
+    num1.to_i * num2.to_i
+  else
+    num1.to_f / num2.to_f
+  end
+
+  # print result
+
+  prompt("The result is #{result}.")
+
+  prompt("Do you want to preform another calculation? Type 'Y' to calculate again.")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
-# print corresponding output
-case op
-when 'a'
-  puts "#{num1} + #{num2} = #{num1.to_i + num2.to_i}"
-when 's'
-  puts "#{num1} - #{num2} = #{num1.to_i - num2.to_i}"
-when 'm'
-  puts "#{num1} x #{num2} = #{num1.to_i * num2.to_i}"
-else
-  puts "#{num1} / #{num2} = #{num1.to_f / num2.to_f}"
-end
+# final goodbye
+prompt("Thanks for using the calculator. Bye bye!")
